a389afbf55b41c1c1b560a4298378d24212d9b8e
Eduardo Kegler <eduardokegler1@gmail.com>
2026-03-09 16:49:20 -0300

    debug: exclude test pages from middleware to isolate auth issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,6 @@ export default NextAuth(authConfig).auth;
 
 export const config = {
   matcher: [
-    "/((?!api|test2|_next/static|_next/image|favicon.ico|icon.svg|manifest.webmanifest|apple-icon|opengraph-image|twitter-image).*)",
+    "/((?!api|test|_next/static|_next/image|favicon.ico|icon.svg|manifest.webmanifest|apple-icon|opengraph-image|twitter-image).*)",
   ],
 };
